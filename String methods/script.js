726de9f87883757267c32929d59149e20c1b2da5
HoriyaDev <you@example.com>
2024-08-05 18:34:15 +0500

    used endWith() methods

--- String methods/script.js
+++ String methods/script.js
@@ -25,23 +25,22 @@ const replaceBtn1 = document.getElementById("replace")
 const replaceBtn2 = document.getElementById("replace2")
 const indexOfBtn = document.getElementById("index-of")
 const endWithBtn = document.getElementById("end-with")
-const startWithBtn = document.getElementById("start-with")
-// startWithBtn.addEventListener("click" , ()=>{
-//     const input = inputField.value
-//     const suffixWord = replaceField.value
-//     const stringLength = parseInt(numberField.value)
-//     const result  = input.endsWith(suffixWord, stringLength)
-//     showDiv.innerHTML=result
-// })
 
-endWithBtn.addEventListener("click", () => {
+endWithBtn.addEventListener("click" , ()=>{
     const input = inputField.value;
     const suffixWord = replaceField.value;
     const stringLength = parseInt(numberField.value, 10);
+
+    
     const truncatedString = isNaN(stringLength) ? input : input.slice(0, stringLength);
+
+    
     const result = truncatedString.endsWith(suffixWord);
+
+    
     showDiv.innerHTML = result;
-});
+})
+
 
 
 
