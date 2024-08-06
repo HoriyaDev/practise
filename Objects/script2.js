bb7a08dda7d0dd45bd420b7b456c3ef5112c7aa3
HoriyaDev <you@example.com>
2024-08-06 18:35:22 +0500

    accessing property dynamically

--- Objects/script2.js
+++ Objects/script2.js
@@ -11,3 +11,15 @@ const object2 = {
 const merge = Object.assign(object1 , object2)
 console.log(merge)
 
+// accessing properties
+ 
+const obj1 = {
+    item : "bike", 
+    color: "red", 
+    number:"32213"
+}
+
+const access = obj1.item
+console.log(access)
+
+
