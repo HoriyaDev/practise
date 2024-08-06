8ca3fe8a5f0b54ea2656e596207eb849a824ceb7
HoriyaDev <you@example.com>
2024-08-06 18:27:08 +0500

    merge 2 objects into 1

---
+++ Objects/script2.js
@@ -0,0 +1,13 @@
+
+// merge two objects
+const object1 = {
+    lead: "naveed",
+    mode:"strict"
+}
+const object2 = {
+    name : "pakistan",
+    id:"44"
+}
+const merge = Object.assign(object1 , object2)
+console.log(merge)
+
